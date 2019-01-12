10d108c93fae27ddd728f08ba9e71ab0dbc71b81
Yujie Li <yjlinotlazy@gmail.com>
2019-01-12 16:28:50 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+required_pkgs = [
+    "json",
+    "pandas",
+    "numpy"
+]
+
+setup (
+    name='restrack',
+    version='0.0.1',
+    packages=['restrack'],
+    install_requires=required_pkgs,
+    entry_points={
+        'console_scripts': ['restrack=restrack.main:main']
+    }
+)
